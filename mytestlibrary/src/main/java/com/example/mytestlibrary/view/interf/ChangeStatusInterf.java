85360c3f9efcec693a0dff5c391ec5de872b0a2a
jackieyao <1428301152@qq.com>
2018-02-01 17:03:02 +0800

    Initial commit

---
+++ mytestlibrary/src/main/java/com/example/mytestlibrary/view/interf/ChangeStatusInterf.java
@@ -0,0 +1,11 @@
+package com.example.mytestlibrary.view.interf;
+
+/**
+ * @author Created by jackieyao on 2018/2/1.
+ */
+
+public interface ChangeStatusInterf {
+    void shiXin();
+    void kongXin();
+    void green();
+}
